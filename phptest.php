6fa9d3e437b4a3ea98df1031f51c5b5a596e5c0d
songyao <646854434@qq.com>
2020-02-17 14:52:15 +0800

    first-project-update

--- phptest.php
+++ phptest.php
@@ -1,15 +1,20 @@
 <?php
-$file = './songyao.txt';
+$file = './input.txt';
 if(file_exists($file)) {
     $handle = fopen($file, 'r');
     $buffer = 1024;
-    $str = fread($handle,$buffer);
+    while(!feof($handle)){//循环读取，直至读取完整个文件
+        $str .= fread($handle,$buffer);
+    }
     $list = explode("\n",$str);
+    $list = array_filter($list);
     $target = $list[0];
     unset($list[0]);
     $list = array_values($list);
     for($i=0;$i<count($list);$i++){
+        if($list[$i] > $target) continue;
         for($j=$i+1;$j<count($list);$j++){
+            if($list[$j] > $target) continue;
             if($list[$i]+$list[$j] == $target){
                 print_r(array($list[$i],$list[$j]));
             }
